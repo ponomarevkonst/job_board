246e9e4b4cfa9fb3787f75206ba2458f44cac4ee
Konstantin Ponomarev <konstantinponomarev@protonmail.com>
2020-10-06 00:59:02 +0300

    add_data command fix

--- jobs/management/commands/add_data.py
+++ jobs/management/commands/add_data.py
@@ -4,13 +4,15 @@ from django.core.management.base import BaseCommand
 from data import specialties, companies, jobs
 from job_board.settings import MEDIA_COMPANY_IMAGE_DIR, MEDIA_SPECIALITY_IMAGE_DIR
 from jobs.models import Specialty, Company, Vacancy
+from django.utils import timezone
 
 
 class Command(BaseCommand):
     help = 'Command for filling database with prepared data'
 
     def handle(self, *args, **options):
-        user = User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')
+        user = User.objects.create_superuser('admin', 'admin@example.com', 'adminpass', last_login=timezone.now())
+        user_id = user.id
         for specialty in specialties:
             picture_name = '/specty_' + specialty['code'] + '.png'
             picture_path = MEDIA_SPECIALITY_IMAGE_DIR + picture_name
@@ -19,11 +21,11 @@ class Command(BaseCommand):
         for company in companies:
             logo_name = company['title'] + '.png'
             logo_path = MEDIA_COMPANY_IMAGE_DIR + '/' + logo_name
-            Company(name=company['title'], employee_count=random.randint(10, 50), user=user, logo=logo_path).save()
+            Company(name=company['title'], employee_count=random.randint(10, 50), user_id=user_id, logo=logo_path).save()
 
         for job in jobs:
             specialty = Specialty.objects.filter(code=job['cat'])[0]
             company = Company.objects.filter(name=job['company'])[0]
             Vacancy(title=job['title'], specialty=specialty, company=company,
-                    description=job['desc'], published_at=job['posted'],
+                    description=job['desc'], published_at=timezone.now(),
                     salary_min=int(job['salary_from']), salary_max=int(job['salary_to'])).save()
