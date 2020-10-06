a66079a5b2d21392b187bf7a5d0bfb578227a7d2
Konstantin Ponomarev <konstantinponomarev@protonmail.com>
2020-10-06 19:57:48 +0300

    populate_db add

--- jobs/management/commands/populate_db.py
+++ jobs/management/commands/populate_db.py
@@ -1,5 +1,9 @@
-""" Вакансии """
-from enum import Enum
+import random
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from job_board.settings import MEDIA_COMPANY_IMAGE_DIR, MEDIA_SPECIALITY_IMAGE_DIR
+from jobs.models import Specialty, Company, Vacancy
+from django.utils import timezone
 
 jobs = [
 
@@ -46,35 +50,26 @@ specialties = [
 
 ]
 
-""" Статусы в формате Enum """
-class EducationChoices(Enum):
-    missing = 'Отсутствует'
-    secondary = 'Среднее'
-    vocational = 'Средне-специальное'
-    incomplete_higher = 'Неполное высшее'
-    higher = 'Высшее'
 
+class Command(BaseCommand):
+    help = 'Command for filling database with prepared data'
 
-class GradeChoices(Enum):
-    intern = 'intern'
-    junior = 'junior'
-    middle = 'middle'
-    senior = 'senior'
-    lead = 'lead'
+    def handle(self, *args, **options):
+        user = User.objects.create_superuser('admin', 'admin@example.com', 'adminpass', last_login=timezone.now())
+        user_id = user.id
+        for specialty in specialties:
+            picture_name = '/specty_' + specialty['code'] + '.png'
+            picture_path = MEDIA_SPECIALITY_IMAGE_DIR + picture_name
+            Specialty(code=specialty['code'], title=specialty['title'], picture=picture_path).save()
 
+        for company in companies:
+            logo_name = company['title'] + '.png'
+            logo_path = MEDIA_COMPANY_IMAGE_DIR + '/' + logo_name
+            Company(name=company['title'], employee_count=random.randint(10, 50), user_id=user_id, logo=logo_path).save()
 
-class SpecialtyChoices(Enum):
-    frontend = 'Фронтенд'
-    backend = 'Бэкенд'
-    gamedev = 'Геймдев'
-    devops = 'Девопс'
-    design = 'Дизайн'
-    products = 'Продукты'
-    management = 'Менеджмент'
-    testing = 'Тестирование'
-
-
-class WorkStatusChoices(Enum):
-    not_in_search = 'Не ищу работу'
-    consideration = 'Рассматриваю предложения'
-    in_search = 'Ищу работу'
+        for job in jobs:
+            specialty = Specialty.objects.filter(code=job['cat'])[0]
+            company = Company.objects.filter(name=job['company'])[0]
+            Vacancy(title=job['title'], specialty=specialty, company=company,
+                    description=job['desc'], published_at=timezone.now(),
+                    salary_min=int(job['salary_from']), salary_max=int(job['salary_to'])).save()
