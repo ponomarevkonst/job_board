16169b8c58e91eeb353c984bcef034ecb1e3421c
Konstantin Ponomarev <ponomarevkonst@gmail.com>
2020-10-06 16:56:06 +0000

    db_population script edit

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,9 @@ then
 
     echo "PostgreSQL started"
 fi
-
+python manage.py makemigrations jobs
+python manage.py makemigrations account
+python manage.py migrate jobs
+python manage.py migrate account
+python manage.py init_data
 exec "$@"
